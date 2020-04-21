02c901c585770c15e673f492435a951456947d6b
Desmond Oben <obendesmond2@gmail.com>
2020-04-21 11:19:01 +0100

    give some margin from header

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -7,7 +7,7 @@ import logo from '../Data/logo.png'
 export default props => {
 
   return(
-    <Container maxWidth="sm">
+    <Container maxWidth="sm" style={{marginTop:'100px'}}>
       <center>
           <img src={logo} style={{height:'200px', marginTop:'50px'}} alt="no logo" />
           <Typography variant="h1">Food Buddy</Typography>
